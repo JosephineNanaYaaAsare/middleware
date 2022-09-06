6deec06d4e46edad4b4669ed0401f1c9c8d545d6
Josephine Nana Yaa Asare <nanayaaasare18@gmail.com>
2022-09-05 20:44:00 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const userRouter = require("./routes/users.route")
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res
+    .status(200)
+    .send("Welcome to my server. Please use/users to get all users")
+})
+
+app.use("/users", userRouter)
+
+app.listen(4000, (err) => {
+    console.log("🚀 server running on port http://localhost:4000")
+
+})
